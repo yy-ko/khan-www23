c512d447ae2f644b954628e651da4fa34e6ebc02
seongeun, ryu <104468077+seongeunryu@users.noreply.github.com>
2022-08-01 17:39:31 +0900

    Update dataloaders.py

--- dataloaders.py
+++ dataloaders.py
@@ -7,21 +7,15 @@ from torchtext.data.utils import get_tokenizer
 from torchtext.vocab import build_vocab_from_iterator
 from torchtext.data.functional import to_map_style_dataset
 
-
 from collections import Counter, OrderedDict
 from typing import Iterable
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-
 def yield_tokens(data_iter, tokenizer):
-    for _, text in data_iter:
-        yield tokenizer(str(text))
-
-def yield_tokens_2(data_iter, tokenizer):
     for _, title, text in data_iter:
-        yield tokenizer(str(text))
         yield tokenizer(str(title))
+        yield tokenizer(str(text))
 
 def preprocess_text(text):
     text = text.str.lower() # lowercase
@@ -31,7 +25,6 @@ def preprocess_text(text):
     text = text.str.replace(r"[^A-Za-z0-9()!?\'\`\"]", " ")
     text = text.str.replace("\s{2,}", " ")
     return text
-
 
 def get_dataloaders(dataset, data_path, batch_size, eval_batch_size, max_len, device):
     """
@@ -55,6 +48,7 @@ def get_dataloaders(dataset, data_path, batch_size, eval_batch_size, max_len, de
     if dataset =='ALLSIDES':
         num_class = 5
         data_path += '/khan_dataset.csv'
+        # data_path += '/news_dataset_test.csv'
 
     elif dataset =='SEMEVAL':
         num_class = 2
@@ -64,24 +58,25 @@ def get_dataloaders(dataset, data_path, batch_size, eval_batch_size, max_len, de
         logging.error('Invalid dataset name!')
         sys.exit(1)
 
-
     # read a dataset file from a local path and pre-process it
     dataset = pd.read_csv(data_path)
     dataset["text"] = preprocess_text(dataset["text"].astype(str))
-    dataset = dataset[['text','label']]
+    dataset = dataset[['text','title','label']]
+    print(dataset.head())
 
     # split a dataset into train/test datasets
     train_df, test_df = train_test_split(dataset, train_size=0.9)
     v_train = train_df.values
-    train_iter = list(map(lambda x: (x.tolist()[1], x.tolist()[0]), v_train))
     v_test = test_df.values
-    test_iter = list(map(lambda x: (x.tolist()[1], x.tolist()[0]), v_test))
-
+    # train_iter = list(map(lambda x: (x.tolist()[1], x.tolist()[0]), v_train))
+    # test_iter = list(map(lambda x: (x.tolist()[1], x.tolist()[0]), v_test))
+    train_iter = list(map(lambda x: (x.tolist()[2], x.tolist()[1], x.tolist()[0]), v_train))
+    test_iter = list(map(lambda x: (x.tolist()[2], x.tolist()[1], x.tolist()[0]), v_test))
+    
     # build vocab
     tokenizer = get_tokenizer('basic_english')
     vocab = build_vocab_from_iterator(yield_tokens(train_iter, tokenizer), specials=['<unk>'])
     vocab.set_default_index(vocab['<unk>'])
-
 
     # (TODO): mapping knowledge and vocab
     # get knowledge entities/relations as a list
@@ -90,16 +85,19 @@ def get_dataloaders(dataset, data_path, batch_size, eval_batch_size, max_len, de
 
 
     def collate_batch(batch): # split a label and text in each row
+        title_pipeline = lambda x: vocab(tokenizer(x))
         text_pipeline = lambda x: vocab(tokenizer(x))
         label_pipeline = lambda x: int(x)
 
-        label_list, text_list = [], []
-        for (_label, _text) in batch:
+        label_list, title_list, text_list = [], [], []
+        for (_label, _title, _text) in batch:
             label_list.append(label_pipeline(_label))
+            title_indices = title_pipeline(_title)
             text_indices = text_pipeline(_text)
 
             # pad/trucate each article embedding according to maximum article length
-            text_size = len(text_indices) 
+            text_size = len(text_indices)
+            title_size = len(title_indices)
             if text_size < max_len:
                 padding_size = max_len - text_size
                 for _ in range(padding_size):
@@ -108,17 +106,26 @@ def get_dataloaders(dataset, data_path, batch_size, eval_batch_size, max_len, de
                 text_indices = text_indices[:max_len]
             else:
                 pass
+            
+            if title_size < max_len:
+                padding_size = max_len - title_size
+                for _ in range(padding_size):
+                    title_indices.append(vocab['unk'])
+            elif title_size > max_len:
+                title_indices = title_indices[:max_len]
+            else:
+                pass
 
+            title_list.append(title_indices)
             text_list.append(text_indices) 
 
-        label_list = torch.tensor(label_list, dtype=torch.int64) 
+        # print(label_list)
+        # print(title_list)
+        # print(text_list)
+        label_list = torch.tensor(label_list, dtype=torch.int64)
+        title_list = torch.tensor(title_list, dtype=torch.int64)
         text_list = torch.tensor(text_list, dtype=torch.int64)
-        return label_list.to(device), text_list.to(device)
-
-
-    # (TODO): chech if train_iter is consumed? need?
-    v_train = train_df.values
-    train_iter = list(map(lambda x: (x.tolist()[1], x.tolist()[0]), v_train))
+        return label_list.to(device), title_list.to(device), text_list.to(device)
 
     train_dataset = to_map_style_dataset(train_iter)
     test_dataset = to_map_style_dataset(test_iter)
@@ -137,174 +144,3 @@ def get_dataloaders(dataset, data_path, batch_size, eval_batch_size, max_len, de
     test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, collate_fn=collate_batch)
 
     return train_dataloader, valid_dataloader, test_dataloader, len(vocab), num_class
-
-
-
-
-
-
-
-
-    #  # title 포함 (잠시 중단)
-    #  elif dataset =='ALLSIDES_2':
-        #  num_class = 5
-        #  tokenizer = get_tokenizer('basic_english')
-        #  # data_path = 'data/khan_dataset.csv'
-        #  data_path = 'data/news_dataset_test.csv'
-        #  dataset = pd.read_csv(data_path)
-        #  dataset["text"]= preprocess_text(dataset["text"].astype(str))
-        #  dataset = dataset[['title','text','label']]
-        
-        #  train_df, test_df = train_test_split(dataset, train_size=0.9)
-        #  v_train = train_df.values
-        #  train_iter = list(map(lambda x: (x.tolist()[2], x.tolist()[0], x.tolist()[1]), v_train))
-        #  v_test = test_df.values
-        #  test_iter = list(map(lambda x: (x.tolist()[2], x.tolist()[0], x.tolist()[1]), v_test))        
-        
-        #  vocab = build_vocab_from_iterator(yield_tokens_2(train_iter, tokenizer), specials=['<unk>'])
-        #  vocab.set_default_index(vocab['<unk>'])
-
-        #  def collate_batch(batch): # split a label and text in each row
-            #  title_pipeline = lambda x: vocab(tokenizer(x))
-            #  text_pipeline = lambda x: vocab(tokenizer(x))
-            #  label_pipeline = lambda x: int(x)
-
-            #  label_list, title_list, text_list = [], [], []
-            #  for (_label, _title, _text) in batch:
-                #  label_list.append(label_pipeline(_label))
-                
-                #  text_indices = text_pipeline(_text)
-                #  title_indices = title_pipeline(_title)
-                #  # pad/trucate each article embedding according to maximum article length
-                #  text_size = len(text_indices)
-                #  title_size = len(title_indices)
-                #  if text_size < max_len:
-                    #  padding_size = max_len - text_size
-                    #  for _ in range(padding_size):
-                        #  text_indices.append(vocab['unk'])
-                #  elif text_size > max_len:
-                    #  text_indices = text_indices[:max_len]
-                #  else:
-                    #  pass
-
-                #  text_list.append(text_indices) 
-            #  return label_list.to(device), text_list.to(device)
-
-
-
-
-
-
-    #  if dataset == 'AGNEWS': # tutorial
-        #  num_class = 4
-        #  train_iter = AG_NEWS(split='train')
-
-        #  tokenizer = get_tokenizer('basic_english')
-        #  vocab = build_vocab_from_iterator(yield_tokens(train_iter, tokenizer), specials=['<unk>'])
-        #  vocab.set_default_index(vocab['<unk>'])
-
-        #  train_iter = AG_NEWS(split='train')
-        #  knowledge = build_knowledge_index_from_iterator(yield_tokens(train_iter, tokenizer))
-
-        #  def collate_batch(batch): # split a label and text in each row
-            #  text_pipeline = lambda x: vocab(tokenizer(x))
-            #  label_pipeline = lambda x: int(x) - 1
-
-            #  label_list, text_list = [], []
-            #  for (_label, _text) in batch: 
-                #  label_list.append(label_pipeline(_label))
-
-                #  text_indices = text_pipeline(_text)
-                #  # pad/trucate each article embedding according to maximum article length
-                #  text_size = len(text_indices) 
-                #  if text_size < max_len:
-                    #  padding_size = max_len - text_size
-                    #  for _ in range(padding_size):
-                        #  text_indices.append(vocab['unk'])
-                #  elif text_size > max_len:
-                    #  text_indices = text_indices[:max_len]
-                #  else:
-                    #  pass
-
-                #  text_list.append(text_indices) 
-
-            #  label_list = torch.tensor(label_list, dtype=torch.int64) 
-            #  text_list = torch.tensor(text_list, dtype=torch.int64)
-
-            #  return label_list.to(device), text_list.to(device)
-
-        #  train_iter, test_iter = AG_NEWS() # train, test
-        #  train_dataset = to_map_style_dataset(train_iter)
-        #  test_dataset = to_map_style_dataset(test_iter)
-
-        #  train_size = int(len(train_dataset) * 0.9)
-        #  val_size = len(train_dataset) - train_size
-        #  train_dataset, val_dataset = random_split(train_dataset, [train_size, val_size])
-
-
-        #  train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_batch)
-        #  valid_dataloader = DataLoader(val_dataset, batch_size=eval_batch_size, shuffle=False, collate_fn=collate_batch)
-        #  test_dataloader = DataLoader(test_dataset, batch_size=eval_batch_size, shuffle=False, collate_fn=collate_batch)
-
-        #  return train_dataloader, valid_dataloader, test_dataloader, len(vocab), num_class
-
-
-
-
-
-
-
-        #  #  print(dataset.head())
-
-        #  train_df, test_df = train_test_split(dataset, train_size=0.9)
-        #  v_train = train_df.values
-        #  train_iter = list(map(lambda x: (x.tolist()[1], x.tolist()[0]), v_train))
-        #  v_test = test_df.values
-        #  test_iter = list(map(lambda x: (x.tolist()[1], x.tolist()[0]), v_test))        
-
-        #  vocab = build_vocab_from_iterator(yield_tokens(train_iter, tokenizer), specials=['<unk>'])
-        #  vocab.set_default_index(vocab['<unk>'])
-
-        #  def collate_batch(batch): # split a label and text in each row
-            #  text_pipeline = lambda x: vocab(tokenizer(x))
-            #  label_pipeline = lambda x: int(x)
-
-            #  label_list, text_list = [], []
-            #  for (_label, _text) in batch:
-                #  label_list.append(label_pipeline(_label))
-                
-                #  text_indices = text_pipeline(_text)
-                #  # pad/trucate each article embedding according to maximum article length
-                #  text_size = len(text_indices) 
-                #  if text_size < max_len:
-                    #  padding_size = max_len - text_size
-                    #  for _ in range(padding_size):
-                        #  text_indices.append(vocab['unk'])
-                #  elif text_size > max_len:
-                    #  text_indices = text_indices[:max_len]
-                #  else:
-                    #  pass
-
-                #  text_list.append(text_indices) 
-            #  label_list = torch.tensor(label_list, dtype=torch.int64) 
-            #  text_list = torch.tensor(text_list, dtype=torch.int64)
-            #  return label_list.to(device), text_list.to(device)
-
-        #  #  v_train = train_df.values
-        #  #  train_iter = list(map(lambda x: (x.tolist()[1], x.tolist()[0]), v_train))
-        #  #  v_test = test_df.values
-        #  #  test_iter = list(map(lambda x: (x.tolist()[1], x.tolist()[0]), v_test))
-
-        #  train_dataset = to_map_style_dataset(train_iter)
-        #  test_dataset = to_map_style_dataset(test_iter)
-
-        #  train_size = int(len(train_dataset) * 0.9)
-        #  val_size = len(train_dataset) - train_size
-        #  train_dataset, val_dataset = random_split(train_dataset, [train_size, val_size])
-
-
-        #  train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, collate_fn=collate_batch)
-        #  valid_dataloader = DataLoader(val_dataset, batch_size=eval_batch_size, shuffle=False, collate_fn=collate_batch)
-        #  test_dataloader = DataLoader(test_dataset, batch_size=eval_batch_size, shuffle=False, collate_fn=collate_batch)
-
-        #  return train_dataloader, valid_dataloader, test_dataloader, len(vocab), num_class
