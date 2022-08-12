d5477c135de01b71acfe600a04e421b3ccca8c76
seongeun, ryu <104468077+seongeunryu@users.noreply.github.com>
2022-08-12 17:41:03 +0900

    Update allsides_xml_parsing.py

--- crawler/allsides_xml_parsing.py
+++ crawler/allsides_xml_parsing.py
@@ -1,9 +1,9 @@
 import xml.etree.ElementTree as ET
 import pandas as pd
 
-xml_path = 'HLSTM-main/data/article_metadata.xml'
+xml_path = 'github/khan/data/allsides_metadata.xml'
 xml_file = open(xml_path, 'rt', encoding='UTF8')
-result_path = 'HLSTM-main/xml_parsing_result'
+result_path = 'github/khan/data'
 
 anno = ET.parse(xml_file)
 xml_articles = anno.getroot()
@@ -23,18 +23,31 @@ for article in xml_articles.findall('article'):
     event = article.find('event').text
     event_list.append(event)
 
-event_list = list(set(event_list))
-print(len(event_list))
+print('======================== event check ========================')
+event_list_deduplication = list(set(event_list))
+print('event count: ',len(event_list))
+print('event types: ',len(event_list_deduplication))
+print('=============================================================')
 
 title_df = pd.DataFrame(title_list, columns = ['title'])
 url_df = pd.DataFrame(url_list, columns = ['url'])
 label_df = pd.DataFrame(label_list, columns = ['label'])
-event_df = pd.DataFrame(event_list, columns = ['event'])
+event_type_df = pd.DataFrame(event_list_deduplication, columns = ['event'])
+
+event_count={}
+for i in event_list:
+    try: event_count[i] += 1
+    except: event_count[i]=1
+print('======================== event count ========================')
+print(pd.DataFrame([event_count]))
+event_count_df = pd.DataFrame([event_count])
+event_count_df.to_csv('/home/yyko/workspace/political_pre/github/khan/data' + '/event_count.csv', index=False, encoding='utf-8-sig')
+print('=============================================================')
+
 
 print(len(title_df))
 print(len(url_df))
 print(len(label_df))
-
 print(title_df.head())
 print(url_df.head())
 print(label_df.head())
