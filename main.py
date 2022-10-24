996d43ed1e1d1dc113c8a1abfb9fc86d24bd7ff9
seongeun, ryu <104468077+seongeunryu@users.noreply.github.com>
2022-10-24 12:29:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,26 @@ from __future__ import print_function
 import argparse
 import random, os, sys
 import numpy as np
+import pandas as pd
 import time
 import logging
 import warnings
 from tqdm import tqdm
-
-import math
 from typing import Tuple
 
 import torch
 from torch import nn, Tensor
 from torch import optim
-
+from scipy.special import softmax
 #  import torch.distributed as dist
 #  from torch.nn.parallel import DistributedDataParallel as DDP
-
 from torch.optim.lr_scheduler import StepLR
 
 import data_utils, models
 from models import KHANModel
+import gc
+gc.collect()
+torch.cuda.empty_cache()
 
 warnings.simplefilter("ignore", UserWarning)
 warnings.simplefilter("ignore", FutureWarning)
@@ -37,11 +38,52 @@ def set_random_seeds(random_seed=0):
     random.seed(random_seed)
     #  pass
 
+parser = argparse.ArgumentParser(description='Parsing parameters')
+
+# for multi-gpu
+#  parser.add_argument("--num_gpus", type=int, default=1, help="The number of GPUs.")
+#  parser.add_argument("--backend", type=str, default='nccl', help="Backend for Distributed PyTorch: nccl, gloo, mpi")
+#  parser.add_argument("--local_rank", type=int, help="Local rank. Necessary for using the torch.distributed.launch utility.")
+parser.add_argument("--gpu_index", type=int, help="GPU index. Necessary for specifying the CUDA device.")
+
+# models & datasets
+parser.add_argument('--model', type=str, default='KHAN', help='Name of Model.')
+parser.add_argument("--embed_size", type=int, default=128, help="Word/Sentence Embedding size.")
+parser.add_argument('--max_sentence', type=int, default=40, help='Maximum Number of Sentences in each document.')
+parser.add_argument('--num_layer', type=int, default=1, help='Number of Transformer Encoder Layers.')
+parser.add_argument('--num_head', type=int, default=4, help='Number of Multihead Attentions.')
+parser.add_argument('--d_hid', type=int, default=256, help='Dimension of a hidden layer.')
+parser.add_argument('--dropout', type=float, default=0.3, help='Dropout probability.')
+parser.add_argument('--alpha', type=float, default=0.5, help='Weight of common knowledge.')
+parser.add_argument('--beta', type=float, default=0.5, help='Weight of political knowledge.')
+
+parser.add_argument('--dataset', type=str, default='SEMEVAL', help='Name of dataset (SEMEVAL, ALLSIDES).')
+parser.add_argument('--data_path', type=str, default='./data', help='Data path.')
+
+parser.add_argument('--save_model', action='store_false', default=False, help='For Saving the current Model')
+parser.add_argument('--model_dir', type=str, default='../trained_models', help='Path for saving the trained model')
+
+# user-defined hyperparameters
+parser.add_argument("--num_epochs", type=int, default=50, help="Number of training epochs.")
+parser.add_argument("--batch_size", type=int, default=16, help="Training batch size.")
+parser.add_argument("--eval_batch_size", type=int, default=16, help="Evaluation and test batch size.")
+parser.add_argument("--learning_rate", type=float, default=0.001, help="Learning rate.")
+parser.add_argument('--seed', type=int, default=1, metavar='S', help='Random seed (default: 1)') # for reproducibility
+
+args = parser.parse_args()
+
+# Get the dataloaders and model
+device = torch.device("cuda:{}".format(args.gpu_index))
+
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# print('Device:', device)
+# print('Current cuda device:', torch.cuda.current_device())
+print('Count of using GPUs:', torch.cuda.device_count())
+
 def evaluate(model, device, dataloader) -> float:
-    model.eval()
+    model.eval() # turn on eval mode
     total_acc = 0
     total_count = 0
-
     with torch.no_grad():
         for idx, (labels, titles, sentences) in enumerate(dataloader):
             predicted_label = model(sentences, titles)
@@ -49,43 +91,123 @@ def evaluate(model, device, dataloader) -> float:
             total_count += labels.size(0)
     return total_acc/total_count
 
+def train_each_fold(train_iter, test_iter, vocab, num_class, knowledge_list, fold_idx, k_folds, sampler):
+    
+    train_iter, test_iter, vocab, num_class, knowledge_list, fold_idx, k_folds, sampler = train_iter, test_iter, vocab, num_class, knowledge_list, fold_idx, k_folds, sampler
+
+    train_data, val_data, test_data = data_utils.get_dataloaders(train_iter, test_iter, vocab, args.batch_size, args.max_sentence, sampler, device)
+
+    nhead = args.num_head # 8
+    d_hid = args.d_hid # 2048
+    dropout = args.dropout # 0.6
+    nlayers = args.num_layer # 4
+    alpha = args.alpha
+    beta = args.beta
+
+    model = KHANModel(len(vocab), args.embed_size, nhead, d_hid, nlayers, dropout, num_class, knowledge_list, alpha, beta)
+    # print(model)
+    params = list(model.parameters())
+    # print(len(params))
+    print(params[0].size())
+    # print(model.shape)
+    
+    model = model.to(device) # model to GPU
+    total = sum([param.nelement() for param in model.parameters()])
+    print("Total params: %.2fM" % (total/1e6))
+    
+    # for using parallel processing
+    # _model = model.cuda()
+    # model = nn.DataParallel(_model).to(device)
+
+    criterion = nn.CrossEntropyLoss() # loss function
+    optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate, weight_decay=0.05) # optimizer
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=5, factor=0.5, verbose=1) # learning rate scheduling
+    
+    start_time = time.time()
+    total_start_time = start_time
+    max_accuracy = 0
+    num_batches = 0
+    
+    for epoch in range(args.num_epochs): 
+        epoch_start_time = time.time() 
+        model.train()  # turn on train mode 
+        train_correct, train_count = 0, 0 
+        start_time = time.time()
+        total_loss = 0.
+
+        # ------------------------ Epoch Start ------------------------ #
+        for idx, (labels, titles, sentences) in enumerate(train_data):
+            optimizer.zero_grad()
+            predicted_labels = model(sentences, titles)
+            loss = criterion(predicted_labels, labels)
+            loss.backward()
+            
+            # Gradient Clipping
+            max_grad_norm = 5
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+            optimizer.step()
+
+            train_correct += (predicted_labels.argmax(1) == labels).sum().item()
+            train_count += labels.size(0) 
+            total_loss += loss.item()
+
+        epoch_loss = total_loss / len(train_data)
+        scheduler.step(epoch_loss)
+        epoch_time = time.time() - epoch_start_time
+        # ------------------------ Epoch End ------------------------ #
+
+        # --------------------------------- EVALUATION --------------------------------- #
+        # ----- at the end of every epoch, evaluating the current model a ccuracy ------ #
+        # ------------------------------------------------------------------------------ #
+        train_accuracy = train_correct / train_count
+        val_accuracy = evaluate(model, device, test_data)
+
+        print('Fold: {:3d} | Epoch: {:3d} | Loss: {:6.4f} | TrainAcc: {:6.4f} | ValAcc: {:6.4f} | Time: {:5.2f}'.format(
+            fold_idx,
+            (epoch+1),
+            total_loss/ len(train_data), 
+            train_accuracy,
+            val_accuracy,
+            epoch_time)
+            )
+
+        # ------------------------- Save Best Model ------------------------- #
+        if val_accuracy > max_accuracy:
+            max_accuracy = val_accuracy
+            #  best_model = copy.deepcopy(model)
+
+        if args.save_model:
+            torch.save(model.state_dict(), args.model_dir)
+
+    # ----------------------------------------------------------------------------#
+    # ----------------------------- End of Training ------------------------------#
+    # ----------------------------------------------------------------------------#
+
+    #  test_accuracy = evaluate(model, device, test_data)
+    fold_train_time = time.time() - total_start_time
+
+    print('')
+    # print('===================================== Training End =====================================')
+    print('================================== FOLD - {:3d} =================================='.format(fold_idx))
+    print('============= Test Accuracy: {:.4f}, Training time: {:.2f} (sec.) ================'.format(max_accuracy, fold_train_time))
+    print('================================================================================')
+    
+    return max_accuracy, fold_train_time
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Parsing parameters')
 
-    # for multi-gpu
-    #  parser.add_argument("--num_gpus", type=int, default=1, help="The number of GPUs.")
-    #  parser.add_argument("--backend", type=str, default='nccl', help="Backend for Distributed PyTorch: nccl, gloo, mpi")
-    #  parser.add_argument("--local_rank", type=int, help="Local rank. Necessary for using the torch.distributed.launch utility.")
-    parser.add_argument("--gpu_index", type=int, help="GPU index. Necessary for specifying the CUDA device.")
+    # ----------------------------------------------------------------------------#
+    # ---------------------------- Main Training Loop ----------------------------#
+    # ----------------------------------------------------------------------------#
+    print('')
+    print('==================================== Training Start ====================================')
 
-    # models & datasets
-    parser.add_argument('--model', type=str, default='KHAN', help='Name of Model.')
-    parser.add_argument("--embed_size", type=int, default=128, help="Word/Sentence Embedding size.")
-    parser.add_argument('--max_sentence', type=int, default=40, help='Maximum Number of Sentences in each document.')
-    parser.add_argument('--num_layer', type=int, default=4, help='Number of Transformer Encoder Layers.')
-    parser.add_argument('--num_head', type=int, default=8, help='Number of Multihead Attentions.')
-    parser.add_argument('--d_hid', type=int, default=2048, help='Dimension of a hidden layer.')
-    parser.add_argument('--dropout', type=float, default=0.1, help='Dropout probability.')
-    parser.add_argument('--alpha', type=float, default=0.5, help='Weight of common knowledge.')
-    parser.add_argument('--beta', type=float, default=0.5, help='Weight of political knowledge.')
-
-    parser.add_argument('--dataset', type=str, default='SEMEVAL', help='Name of dataset (SEMEVAL, ALLSIDES).')
-    parser.add_argument('--data_path', type=str, default='./data', help='Data path.')
-
-    parser.add_argument('--save_model', action='store_false', default=False, help='For Saving the current Model')
-    parser.add_argument('--model_dir', type=str, default='../trained_models', help='Path for saving the trained model')
-
-    # user-defined hyperparameters
-    parser.add_argument("--num_epochs", type=int, default=100, help="Number of training epochs.")
-    parser.add_argument("--batch_size", type=int, default=64, help="Training batch size.")
-    parser.add_argument("--eval_batch_size", type=int, default=16, help="Evaluation and test batch size.")
-    parser.add_argument("--learning_rate", type=float, default=0.1, help="Learning rate.")
-    parser.add_argument('--seed', type=int, default=1, metavar='S', help='Random seed (default: 1)') # for reproducibility
-
-    args = parser.parse_args()
-
+    data_utils.KFold_data_preprocessing(args.dataset, args.data_path)
+    
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    
     #  dist.init_process_group(backend='nccl')
     #  world_size = dist.get_world_size()
     #  rank = dist.get_rank()
@@ -112,105 +234,6 @@ def main():
     print('  - LEARNING RATE = ' + str(args.learning_rate))
     print('   ')
     # add more if needed
-
-
-    # Get the dataloaders and model
-    #  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    device = torch.device("cuda:{}".format(args.gpu_index)) 
-
-    train_iter, test_iter, vocab, num_class, knowledge_list = data_utils.data_preprocessing(args.dataset, args.data_path)
-    train_data, val_data, test_data = data_utils.get_dataloaders(train_iter, test_iter, vocab, args.batch_size, args.max_sentence, device)
-
-    nhead = args.num_head # 8
-    d_hid = args.d_hid # 2048
-    dropout = args.dropout # 0.6
-    nlayers = args.num_layer # 4
-    alpha = args.alpha
-    beta = args.beta
-
-    model = KHANModel(len(vocab), args.embed_size, nhead, d_hid, nlayers, dropout, num_class, knowledge_list, alpha, beta)
-    model = model.to(device) # model to GPU
-
-    criterion = nn.CrossEntropyLoss() # loss function
-    #  optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate, momentum=0.9, weight_decay=0) # optimizer
-    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate, weight_decay=5e-4) # optimizer
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=10) # learning rate scheduling
-
-    start_time = time.time()
-    total_start_time = start_time
-    max_accuracy = 0
-    num_batches = 0
-
-    # ----------------------------------------------------------------------------#
-    # ---------------------------- Main Training Loop ----------------------------#
-    # ----------------------------------------------------------------------------#
-    print('')
-    print('==================================== Training Start ====================================')
-
-    #  for epoch in tqdm(range(args.num_epochs)): 
-    for epoch in range(args.num_epochs): 
-        epoch_start_time = time.time() 
-        model.train()  # turn on train mode 
-        train_correct, train_count = 0, 0 
-        start_time = time.time()
-        total_loss = 0.
-
-        # ------------------------ Epoch Start ------------------------ #
-        for idx, (labels, titles, sentences) in enumerate(train_data):
-            optimizer.zero_grad()
-            #  predicted_labels = model(texts)
-            predicted_labels = model(sentences, titles)
-            loss = criterion(predicted_labels, labels)
-            loss.backward()
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 0.1)
-            optimizer.step()
-
-            train_correct += (predicted_labels.argmax(1) == labels).sum().item()
-            train_count += labels.size(0) 
-            total_loss += loss.item()
-
-        epoch_loss = total_loss / len(train_data)
-        scheduler.step(epoch_loss)
-        epoch_time = time.time() - epoch_start_time
-        # ------------------------ Epoch End ------------------------ #
-
-        # --------------------------------- EVALUATION --------------------------------- #
-        # ----- at the end of every epoch, evaluating the current model a ccuracy ------ #
-        # ------------------------------------------------------------------------------ #
-        train_accuracy = train_correct / train_count
-        val_accuracy = evaluate(model, device, test_data)
-        # val_accuracy = evaluate(model, device, val_data)
-
-        print('Epoch: {:3d} | Loss: {:6.4f} | TrainAcc: {:6.4f} | ValAcc: {:6.4f} | Time: {:5.2f}'.format(
-            (epoch+1),
-            total_loss/ len(train_data), 
-            train_accuracy,
-            val_accuracy,
-            epoch_time)
-            )
-
-        # ------------------------- Save Best Model ------------------------- #
-        if val_accuracy > max_accuracy:
-            max_accuracy = val_accuracy
-            #  best_model = copy.deepcopy(model)
-
-        if args.save_model:
-            torch.save(model.state_dict(), args.model_dir)
-
-    # ----------------------------------------------------------------------------#
-    # ----------------------------- End of Training ------------------------------#
-    # ----------------------------------------------------------------------------#
-
-    #  test_accuracy = evaluate(model, device, test_data)
-    total_train_time = time.time() - total_start_time
-
-    print('')
-    print('===================================== Training End =====================================')
-    print('============= Test Accuracy: {:.4f}, Total training time: {:.2f} (sec.) ================'.format(max_accuracy, total_train_time))
-    print('========================================================================================')
-
-
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
+    
     main()
+    
